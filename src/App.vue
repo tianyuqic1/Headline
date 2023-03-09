493f9b224c0e65dd3f8d3fdc4d4ff6eba2bc2dd7
Kevin <1253148248@qq.com>
2023-03-09 22:49:20 +0800

    第一次提交dist

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <router-view></router-view>
+<van-tabbar route>
+  <van-tabbar-item icon="home-o" replace to="/">主页</van-tabbar-item>
+  <van-tabbar-item icon="user-o" replace to="/me">我的</van-tabbar-item>
+</van-tabbar>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+
 }
+</script>
 
-nav {
-  padding: 30px;
+<style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
